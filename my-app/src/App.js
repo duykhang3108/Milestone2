87021ba15231f8871107ce244ff68590229ce03b
Duy Khang <alucard310800@gmail.com>
2020-08-03 13:56:39 +0700

    this is my first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Register from './pages/Register';
+import Draft from './pages/Draft';
+import Profile from './pages/Profile';
+
+function App() {
+  return (
+    <div>
+        {/* <Register/> */}
+         <Profile/>
+    </div>
+    
+  );
+}
+
+export default App;
