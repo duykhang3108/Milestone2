2fdf5cad2ab804ebde640c63e04df85db1b879e8
duykhang3108 <s3753740@rmit.edu.vn>
2020-08-06 11:51:04 +0700

    App.js change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,14 +4,21 @@ import './App.css';
 import Register from './pages/Register';
 import Draft from './pages/Draft';
 import Profile from './pages/Profile';
+import { BrowserRouter as Router, Link, Route, Switch, Redirect } from 'react-router-dom'
+import RouterTest from './RouterTest';
 
 function App() {
   return (
     <div>
-        {/* <Register/> */}
-         <Profile/>
+      <Router>
+        {/* <Route exact path='/RouterTest' component={RouterTest} /> */}
+        <Route exact path='/' component={Register}/>
+        <Route path={`/Profile/:userName`} render={(props) =>
+            <Profile {...props} />
+          } />
+      </Router>
     </div>
-    
+
   );
 }
 
